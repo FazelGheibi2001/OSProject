332df824afbcdbda70cb9d3c80bdd953b2a4e75b
Matin <matinvahediyan@gmail.com>
2021-12-27 17:27:35 +0330

    x

--- src/com/company/domain/Customer.java
+++ src/com/company/domain/Customer.java
@@ -1,12 +1,26 @@
 package com.company.domain;
 
-public class Customer {
+import com.company.banker.BankerWork;
+
+import java.util.Random;
+
+public class Customer extends Thread {
     private String firstName;
     private String lastName;
     private BankAccount account;
     private String nationalId;
+    private Random r;
+    private boolean wantsBanker;
+    private BankerWork bankerWork;
 
     public Customer() {
+    }
+
+    public Customer(BankerWork bankerWork, String firstName) {
+        this.firstName = firstName;
+        this.bankerWork = bankerWork;
+        wantsBanker = true;
+        r = new Random();
     }
 
     public Customer(String firstName, String lastName, BankAccount account, String nationalId) {
@@ -14,6 +28,29 @@ public class Customer {
         this.lastName = lastName;
         this.account = account;
         this.nationalId = nationalId;
+        wantsBanker = true;
+        r = new Random();
+    }
+
+    public void run() {
+        wasteTime();
+        bankerWork.customerReady(this);
+    }
+
+    public boolean wantsHaircut() {
+        return wantsBanker;
+    }
+
+    public void wantsToLeave() {
+        wantsBanker = false;
+    }
+
+    public void wasteTime() {
+        try {
+            this.sleep(Math.abs(r.nextInt(100000)));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getFirstName() {
